050043cfa380930df7286222703bc3440750bb32
TusharJade <79801546+TusharJade@users.noreply.github.com>
2021-09-27 17:10:16 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h2 className="heading">Travel recomendations</h2>
+
+      <p className="para">
+        Here are some of my favorite places which I was visited in my life
+      </p>
+    </div>
+  );
+}
